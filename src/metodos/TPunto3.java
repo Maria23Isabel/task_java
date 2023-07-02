be276de436ca682dec942e6751949f8199ebdac6
Maria <desarrollador8@jycleadingcorp.com>
2023-07-02 18:02:36 -0500

    Alvaro xd

--- src/metodos/TPunto3.java
+++ src/metodos/TPunto3.java
@@ -1,9 +1,40 @@
 package metodos;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Scanner;
+import modelo.Producto;
+
 /**
  * 
  * @author maria
  */
 public class TPunto3 {
+public static void main(String[] args) {
+        Hashtable<Object, Producto> ht = new Hashtable<>();
 
+        Producto emp = new Producto();
+        ht.put(emp.getProductosAbarrotes(), emp);
+
+        emp = new Producto("AB001", "tt", 55, 12, "a", "s", 12);
+        ht.put(emp.getCodigo(), emp);
+
+        emp = new Producto("b", "rr", 44, 12, "a", "s", 12);
+        ht.put(emp.getCodigo(), emp);
+
+        emp = new Producto("c", "ss", 3, 12, "a", "s", 12);
+        ht.put(emp.getCodigo(), emp);
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese codigo a buscar:");
+        String cod = entrada.next();
+        System.out.println("" + ht.get(cod));
+
+        Enumeration lista = ht.keys();
+        while (lista.hasMoreElements()) {
+            Object clave = (Object) lista.nextElement();
+            Producto empx = ht.get(clave);
+            System.out.println("" + empx.getCodigo() + "" + empx.getNombre());
+        }
+}
 }
